de43bb0af3292cf281cf8b5dc006141a3b64756f
Charlotte Récapet <22002122+crecapet@users.noreply.github.com>
2019-04-02 16:04:08 +0200

    Minor updates

--- run_Salmo_salar.m
+++ run_Salmo_salar.m
@@ -4,7 +4,7 @@ addpath(genpath('C:/Users/crecapet/Documents/Recherche/Modelisation/DEB/DEB scho
 global pets
 
 pets = {'Salmo_salar'};
-check_my_pet(pets); 
+% check_my_pet(pets); 
 
 % Read about how to set estimation and output options (estim_options) on the online
 % manual: http://www.debtheory.org/wiki/index.php?title=Run_file
@@ -16,7 +16,7 @@ estim_options('max_fun_evals',5e3);
 estim_options('pars_init_method', 2);
 estim_options('loss_function', 'sb');
 estim_options('report', 1);
-estim_options('results_output', 4);
+estim_options('results_output', 3);
 estim_options('method', 'nm');
 
 estim_pars; 
