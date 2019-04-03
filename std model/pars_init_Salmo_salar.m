facf71b711e2a712c2d3953d9be36048048e5a01
Charlotte Récapet <22002122+crecapet@users.noreply.github.com>
2019-04-04 01:52:47 +0200

    Updates in std model

--- std model/pars_init_Salmo_salar.m
+++ std model/pars_init_Salmo_salar.m
@@ -10,7 +10,7 @@ par.z    = 14.48;      free.z     = 1;   units.z     = '-';        label.z     =
 par.F_m   = 6.5;   free.F_m   = 0;   units.F_m   = 'l/d.cm^2'; label.F_m   = '{F_m}, max spec searching rate';
 par.kap_X = 0.8;   free.kap_X = 0;   units.kap_X = '-';        label.kap_X = 'digestion efficiency of food to reserve';
 par.kap_P = 0.1;   free.kap_P = 0;   units.kap_P = '-';        label.kap_P = 'faecation efficiency of food to faeces';
-par.v     = 0.02;  free.v     = 1;   units.v     = 'cm/d';     label.v     = 'energy conductance';
+par.v     = 0.02;  free.v     = 0;   units.v     = 'cm/d';     label.v     = 'energy conductance';
 par.kap   = 0.9;   free.kap   = 1;   units.kap   = '-';        label.kap   = 'allocation fraction to soma';
 par.kap_R = 0.95;  free.kap_R = 0;   units.kap_R = '-';        label.kap_R = 'reproduction efficiency';
 par.p_M   = 18;    free.p_M   = 1;   units.p_M   = 'J/d.cm^3'; label.p_M   = '[p_M], vol-spec somatic maint';
