5d27017736ea3808d3b3d56323492565a450c681
Charlotte Récapet <22002122+crecapet@users.noreply.github.com>
2019-04-03 12:29:14 +0200

    Added std model

--- std model/run_Salmo_salar.m
+++ std model/run_Salmo_salar.m
@@ -10,13 +10,13 @@ pets = {'Salmo_salar'};
 % manual: http://www.debtheory.org/wiki/index.php?title=Run_file
 
 estim_options('default'); 
-estim_options('max_step_number',5e4); 
+estim_options('max_step_number',5e2); 
 estim_options('max_fun_evals',5e3);  
 
 estim_options('pars_init_method', 1);
 estim_options('loss_function', 'sb');
 estim_options('report', 1);
-estim_options('results_output', 3);
+estim_options('results_output', 0);
 estim_options('method', 'nm');
 
 estim_pars; 
